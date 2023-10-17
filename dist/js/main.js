b54703d730a1a93fb81cc0df7caf564df6144213
Hannah <118190915+hannadzenis@users.noreply.github.com>
2023-10-17 13:40:23 +0200

    feat: validations corrected & message about successful registration added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,13 @@
 //Walidacje formularza
 
-const inputs = document.querySelectorAll('#input')
+const inputs = document.querySelectorAll('#input');
+const nameInput = document.querySelectorAll('.name');
 const formSubmitButton = document.querySelector('.button');
 const validationWindow  = document.querySelector('.validation-alert');
 const emailInput = document.querySelector('.email');
+const successMessage = document.querySelector('.success-message');
+const overlay = document.querySelector('.overlay');
+
 
 const checkInputsForEmptyness = () => {
     formSubmitButton.addEventListener('click', (e) => {
@@ -17,12 +21,14 @@ const checkInputsForEmptyness = () => {
                 highlightRemove(input);
             }
         })
-        validateEmail(emailInput.value) ? alert('true') : highlightWrongInput(emailInput);
+        validateEmail(emailInput.value) ? successfulFormMessage() : highlightWrongInput(emailInput)&openValidationWindow();
     })
 }
 
+
 function openValidationWindow(){
     validationWindow.classList.add('active-window');
+    setTimeout(closeValidationWindow, 5000);
 }
 
 function closeValidationWindow(){
@@ -31,12 +37,10 @@ function closeValidationWindow(){
 
 function highlightWrongInput(item){
     item.classList.add('highlight-red');
-    // console.log(item, item.classList);
 }
 
 function highlightRemove(item){
     item.classList.remove('highlight-red');
-    // console.log(item, item.classList);
 }
 
 function validateEmail(email) {
@@ -44,6 +48,30 @@ function validateEmail(email) {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 };
+
+//successful registration message
+function successfulFormMessage(){
+    //just in case name corrections
+    const name = nameInput[0].value.charAt(0).toUpperCase() + nameInput[0].value.slice(1);
+    const surname = nameInput[1].value.charAt(0).toUpperCase() + nameInput[1].value.slice(1);
+
+    const userName = name + " " + surname;
+
+    //open message
+    if(checkInputsForEmptyness){
+        document.getElementById("success").innerHTML =`${userName}, thank You for your trust!`;
+        successMessage.classList.add('active');
+        overlay.setAttribute('style', 'display: block');
+    }
+
+    //close message
+    const btnClose = document.querySelector('.success-message__button');
+    btnClose.addEventListener('click', ()=>{
+        successMessage.classList.remove('active');
+        overlay.removeAttribute('style', 'display: block');
+        document.getElementById('form').reset()
+    })
+}
 
 
 checkInputsForEmptyness()
